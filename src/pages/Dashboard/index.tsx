af2d34671fcf0d9098cd36898d18743e7b9794e3
Camila Sales <salescamila1@gmail.com>
2020-07-31 00:16:01 -0400

    Estilizando os ítens do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,54 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/16194059?s=460&u=48eb2fb0ab72c31eca36377110f8a3ef72a2a685&v=4"
+            alt="Camila Sales"
+          />
+          <div>
+            <strong>salescamila/gostack</strong>
+            <p>
+              Índice dos repositórios criados durante o Bootcamp GoStack da
+              @Rocketseat
+            </p>
+          </div>
+
+          <FiChevronRight />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/16194059?s=460&u=48eb2fb0ab72c31eca36377110f8a3ef72a2a685&v=4"
+            alt="Camila Sales"
+          />
+          <div>
+            <strong>salescamila/gostack</strong>
+            <p>
+              Índice dos repositórios criados durante o Bootcamp GoStack da
+              @Rocketseat
+            </p>
+          </div>
+
+          <FiChevronRight />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/16194059?s=460&u=48eb2fb0ab72c31eca36377110f8a3ef72a2a685&v=4"
+            alt="Camila Sales"
+          />
+          <div>
+            <strong>salescamila/gostack</strong>
+            <p>
+              Índice dos repositórios criados durante o Bootcamp GoStack da
+              @Rocketseat
+            </p>
+          </div>
+
+          <FiChevronRight />
+        </a>
+      </Repositories>
     </>
   );
 };
