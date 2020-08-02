fea36eaf70afdb19dca31257560bc0c2d77452b6
Camila Sales <salescamila1@gmail.com>
2020-08-02 17:18:01 -0400

    Salvando no storage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -18,7 +18,25 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    // Recupera do Local Storage os repositórios salvos
+    const storadedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories',
+    );
+
+    if (storadedRepositories) {
+      return JSON.parse(storadedRepositories);
+    }
+    return [];
+  });
+
+  // Salvando no Local Storage o novo repositório
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   // Adição de um novo repositório
   async function handleAddRepository(
