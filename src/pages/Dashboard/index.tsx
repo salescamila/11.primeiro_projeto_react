354878256215135e36002895163ba59255788231
Camila Sales <salescamila1@gmail.com>
2020-08-01 23:06:00 -0400

    Conectando a api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,67 +1,70 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  // Adição de um novo repositório
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    // Consumir API do Github
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    // Salvar novo repositório no estado
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/16194059?s=460&u=48eb2fb0ab72c31eca36377110f8a3ef72a2a685&v=4"
-            alt="Camila Sales"
-          />
-          <div>
-            <strong>salescamila/gostack</strong>
-            <p>
-              Índice dos repositórios criados durante o Bootcamp GoStack da
-              @Rocketseat
-            </p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>
+                Índice dos repositórios criados durante o Bootcamp GoStack da
+                @Rocketseat
+              </p>
+            </div>
 
-          <FiChevronRight />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/16194059?s=460&u=48eb2fb0ab72c31eca36377110f8a3ef72a2a685&v=4"
-            alt="Camila Sales"
-          />
-          <div>
-            <strong>salescamila/gostack</strong>
-            <p>
-              Índice dos repositórios criados durante o Bootcamp GoStack da
-              @Rocketseat
-            </p>
-          </div>
-
-          <FiChevronRight />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/16194059?s=460&u=48eb2fb0ab72c31eca36377110f8a3ef72a2a685&v=4"
-            alt="Camila Sales"
-          />
-          <div>
-            <strong>salescamila/gostack</strong>
-            <p>
-              Índice dos repositórios criados durante o Bootcamp GoStack da
-              @Rocketseat
-            </p>
-          </div>
-
-          <FiChevronRight />
-        </a>
+            <FiChevronRight />
+          </a>
+        ))}
       </Repositories>
     </>
   );
